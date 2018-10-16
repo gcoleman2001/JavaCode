31cfe5b2b420596b5f7842cbb41f4317f920bc04
Coleman <gcoleman@meteor2000.ie>
2018-10-16 14:25:35 +0100

    initialTest

---
+++ app.java
@@ -0,0 +1,10 @@
+public class app{
+	public static void main(String[] args){
+		app App = new app();
+		System.out.println("Tester print");
+		App.anotherMethod();
+	}
+	public void anotherMethod(){
+		System.out.println("Inside another method");
+	}
+}
